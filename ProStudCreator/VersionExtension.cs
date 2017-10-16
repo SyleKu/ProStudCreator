307ecdae64162fdc10a50d00701115873f64f051
Jonas Suter <jonas.suter@fhnw.ch>
2017-10-16 17:23:20 +0200

    completed last commit

--- ProStudCreator/VersionExtension.cs
+++ ProStudCreator/VersionExtension.cs
@@ -6,8 +6,8 @@ using Microsoft.Ajax.Utilities;
 
 namespace ProStudCreator
 {
-    public partial class Project { 
-
+    public partial class Project {
+        public String StateColor { get; set; }
         public String StateAsString
         {
 
@@ -16,15 +16,24 @@ namespace ProStudCreator
                 switch (State)
                 {
                     case ProjectState.InProgress:
+                        StateColor = "#EFF3FB"; 
                         return "In Bearbeitung";
+
                     case ProjectState.Published:
+                        StateColor = "#A9F5A9";
                         return "Veröffentlicht";
+
                     case ProjectState.Rejected:
+                        StateColor = "#F5A9A9";
                         return "Abgelehnt";
+
                     case ProjectState.Submitted:
+                        StateColor = "#ffcc99";
                         return "Eingereicht";
+
                     case ProjectState.Deleted:
                         return "Gelöscht";
+
                     default:
                         return "Error!";
 
